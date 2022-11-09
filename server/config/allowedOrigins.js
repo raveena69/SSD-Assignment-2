e3d44fe2afe0b4d9cde8a8dfe4b341b3d23a06ce
Raveena Amarasiriwardena <58651579+raveena69@users.noreply.github.com>
2022-11-09 23:08:07 +0530

    Add SSL-TLS Certificate

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://localhost:3000',
+    'https://www.spacecompany.com'
 ]
 
 module.exports = allowedOrigins
